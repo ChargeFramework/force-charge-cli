2f330ed566af4138505c7ed677dc686f9ff3e80f
Dang Mai <contact@dangmai.net>
2017-12-11 01:04:23 -0500

    Link dependency packages with current project

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,11 +1,14 @@
-import path from "path";
 import fs from "fs";
-import util from "util";
+import os from "os";
+import path from "path";
+import { promisify } from "util";
 
 import { Command } from "cli-engine-heroku";
+import Rx from "rxjs/Rx";
+
 import NpmCommand from "./npm";
 
-const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
 export default class BuildCommand extends Command {
   static topic = "charge:project";
@@ -21,17 +24,31 @@ export default class BuildCommand extends Command {
     await npm.invokeNpmCommand(["dedupe"]);
 
     try {
-      const packageJson = await this.getPackageJsonContent();
-      if (!packageJson.charge) {
-        throw new Error(
-          "package.json file does not contain charge framework configuration!",
-        );
-      }
-      const packageTreeContent = await npm.invokeNpmCommand(["ls", "--json"], {
-        silent: true,
-      });
-      const packageTree = JSON.parse(packageTreeContent);
-      this.out.log(JSON.stringify(packageTree, null, 2));
+      const dependencyPaths = await this.getDependencyPaths();
+
+      const chargePackagePaths$ = this.getChargePackages$(
+        process.cwd(),
+        dependencyPaths,
+      );
+      const packageDirectories$ = this.getPackageDirectories$(
+        chargePackagePaths$,
+      );
+      const sfdxProjectJsonPath = path.join(process.cwd(), "sfdx-project.json");
+      const sfdxProject = await this.getJsonContent(sfdxProjectJsonPath);
+      packageDirectories$
+        .toArray()
+        .map(packageDirectories => {
+          sfdxProject.packageDirectories = sfdxProject.packageDirectories.concat(
+            packageDirectories,
+          );
+          return sfdxProject;
+        })
+        .do(sfdxProjectConfigs =>
+          Rx.Observable.fromPromise(
+            this.writeJsonFile(sfdxProjectJsonPath, sfdxProjectConfigs),
+          ),
+        )
+        .subscribe();
     } catch (e) {
       if (e.code === "ENOENT") {
         throw new Error(
@@ -43,11 +60,75 @@ export default class BuildCommand extends Command {
     }
   }
 
-  async getPackageJsonContent() {
-    const currentDir = process.cwd();
-    const projectConfigPath = path.join(currentDir, "package.json");
+  getChargePackages$(cwd, dependencyPaths) {
+    const dependencies$ = Rx.Observable.of(cwd).expand(cwd => {
+      return Rx.Observable.fromPromise(this.getPackageJsonContent(cwd)).flatMap(
+        packageJson => {
+          if (!packageJson.charge || !packageJson.charge.packages) {
+            return Rx.Observable.empty();
+          }
+          return Rx.Observable.from(packageJson.charge.packages).flatMap(
+            chargePackage => {
+              const packagePaths = dependencyPaths.filter(path =>
+                path.endsWith(chargePackage),
+              );
+              if (packagePaths.length > 1) {
+                this.out.warn(
+                  `Multiple versions of ${
+                    chargePackage
+                  } found. Using the first one!`,
+                );
+              }
+              return Rx.Observable.of(packagePaths[0]);
+            },
+          );
+        },
+      );
+    });
+    return dependencies$.skip(1); // Since the first one is always the "self" project
+  }
 
-    const configContent = await readFileAsync(projectConfigPath, "utf8");
-    return JSON.parse(configContent);
+  getPackageDirectories$(chargePackagePaths$) {
+    const cwd = process.cwd();
+    return chargePackagePaths$
+      .flatMap(packagePath =>
+        Rx.Observable.fromPromise(
+          this.getJsonContent(path.join(packagePath, "sfdx-project.json")),
+        )
+          .flatMap(projectConfig =>
+            Rx.Observable.from(projectConfig.packageDirectories),
+          )
+          .map(packageDirectoryPath =>
+            path.resolve(packagePath, packageDirectoryPath.path),
+          ),
+      )
+      .map(packagePath => path.relative(cwd, packagePath))
+      .map(packagePath => ({ path: packagePath }));
+  }
+
+  async getDependencyPaths() {
+    const npm = new NpmCommand();
+    const packageTreeContent = await npm.invokeNpmCommand(
+      ["ls", "--parseable"],
+      {
+        silent: true,
+      },
+    );
+    const packages = packageTreeContent.split(os.EOL);
+    packages.shift(); // since the first element is the package itself
+    return packages;
+  }
+
+  async getPackageJsonContent(cwd) {
+    const projectConfigPath = path.join(cwd, "package.json");
+    return require(projectConfigPath);
+  }
+
+  async getJsonContent(filePath) {
+    return require(filePath);
+  }
+
+  async writeJsonFile(filePath, jsonContent) {
+    await writeFileAsync(filePath, JSON.stringify(jsonContent, null, 2));
   }
 }
